f0bfa14759707bb5b0ba141da9c507eb48ee7f58
antonkazakov1 <antonanton08@gmail.com>
2016-11-21 11:41:15 +0300

    added sql queries

---
+++ src/test/java/dao/UserDAOImplTest.java
@@ -0,0 +1,42 @@
+package dao;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by antonkazakov on 21.11.16.
+ */
+public class UserDAOImplTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void findAll() throws Exception {
+
+    }
+
+    @Test
+    public void find() throws Exception {
+
+    }
+
+    @Test
+    public void save() throws Exception {
+
+    }
+
+    @Test
+    public void delete() throws Exception {
+
+    }
+
+    @Test
+    public void update() throws Exception {
+
+    }
+
+}
